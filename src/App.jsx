defdcb43ee61c68b482dbca649a3677e9a3964a9
KarnamShyam1947 <karnamshyamgithub@gmail.com>
2024-09-03 22:15:41 +0530

    learned: useState hook(counter example)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import AppFooter from "./components/AppFooter"
 import AppHeader from "./components/AppHeader"
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  }
+  
+  const reset = () => {
+    setCount(0);
+  }
+  
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
 
   return (
     <>
       <AppHeader />
 
       <div className="main">
-        <h1>Hello, World</h1>
+        <div>
+          <h1>Count : {count}</h1>
+          <button onClick={increase}>+</button>
+          <button onClick={decrease}>-</button>
+          <button onClick={reset}>reset</button>
+        </div>
       </div>
 
       <AppFooter />
